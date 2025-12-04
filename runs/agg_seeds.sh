5b9b844b691878c2d01218004def3431aa987f46
wndudwkd003 <ymail3@naver.com>
2025-12-04 09:10:42 +0000

    ㅁㄴㅇ

--- runs/agg_seeds.sh
+++ runs/agg_seeds.sh
@@ -3,6 +3,6 @@
 # chmod +x runs/agg_seeds.sh
 
 RUN_JSON="outputs_auto_run/2025-12-04_08-31-10_lightgbm_0.0_to_0.0_0.0_step_single_mcar_zero_auto.json"
-TEST_MODE="0"
+TEST_MODE="1"
 
 python -m scripts.agg_seeds "$RUN_JSON" "$TEST_MODE"
--- runs/auto_run.sh
+++ runs/auto_run.sh
@@ -15,7 +15,7 @@ if [ "$MODE" = "test" ]; then
 fi
 
 # train 모드일 때 seeds 정의
-SEEDS=(42 234 2025 6652 45321)
+SEEDS=(42 234 2025 6652 45321) # (42 234 2025 6652 45321)
 
 python -m scripts.auto_run \
     --mode train \
